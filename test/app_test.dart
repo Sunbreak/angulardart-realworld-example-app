94fd493eec60771a7ab05b21e4fdbe69f26179ea
Sunbreak <sunbreak.wang@gmail.com>
2020-08-11 18:53:37 +0800

    Initial commit
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ test/app_test.dart
@@ -0,0 +1,31 @@
+import 'package:angular_test/angular_test.dart';
+@TestOn('browser')
+import 'package:realworld/app_component.dart';
+import 'package:realworld/app_component.template.dart' as ng;
+import 'package:test/test.dart';
+
+void main() {
+  final testBed =
+      NgTestBed.forComponent<AppComponent>(ng.AppComponentNgFactory);
+  NgTestFixture<AppComponent> fixture;
+
+  setUp(() async {
+    fixture = await testBed.create();
+  });
+
+  tearDown(disposeAnyRunningTest);
+
+  test('Default greeting', () {
+    expect(fixture.text, 'Hello Angular');
+  });
+
+  test('Greet world', () async {
+    await fixture.update((c) => c.name = 'World');
+    expect(fixture.text, 'Hello World');
+  });
+
+  test('Greet world HTML', () {
+    final html = fixture.rootElement.innerHtml;
+    expect(html, '<h1>Hello Angular</h1>');
+  });
+}
